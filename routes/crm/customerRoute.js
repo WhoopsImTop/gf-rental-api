8c3eab9518b54a3adb00960420f2b6f97e4af7bc
Elias Englen <eliasenglen2@gmail.com>
2025-01-23 20:56:05 +0100

    add remove User from customer Route

--- routes/crm/customerRoute.js
+++ routes/crm/customerRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createCustomer, findAllCustomers, findOneCustomer, updateCustomer, setCustomerAction, deleteCustomer, assignUserToCustomer } = require("../../controllers/crm/customerController");
+const { createCustomer, findAllCustomers, findOneCustomer, updateCustomer, setCustomerAction, deleteCustomer, assignUserToCustomer, removeUserFromCustomer } = require("../../controllers/crm/customerController");
 
 router.post("/", createCustomer);
 router.get("/", findAllCustomers);
