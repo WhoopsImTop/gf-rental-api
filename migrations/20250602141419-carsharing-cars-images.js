3283a6a4b495a1034a50a49cecf6a5e361fd9ca9
WhoopsImTop <eliasenglen2@gmail.com>
2025-07-02 15:08:39 +0200

    add new carsharing details

--- migrations/20250602141419-carsharing-cars-images.js
+++ migrations/20250602141419-carsharing-cars-images.js
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: "carsharingCars",
+          model: "CarsharingCars",
           key: "id",
         },
         onUpdate: "CASCADE",
