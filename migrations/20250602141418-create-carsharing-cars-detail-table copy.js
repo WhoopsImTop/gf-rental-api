0d236750a368d1a7b0509ad5e837fc8b3beaee7f
WhoopsImTop <eliasenglen2@gmail.com>
2025-07-03 09:55:58 +0200

    update carsharingCar patch function

--- migrations/20250602141418-create-carsharing-cars-detail-table copy.js
+++ migrations/20250602141418-create-carsharing-cars-detail-table copy.js
@@ -10,7 +10,7 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
       },
       subline: {
         type: Sequelize.TEXT,
