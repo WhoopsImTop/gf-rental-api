1436e5d592f53b8c8fd55364aef0d4a4a7e88785
WhoopsImTop <eliasenglen2@gmail.com>
2025-07-08 07:57:03 +0200

    add new

--- models/carsharingcar.js
+++ models/carsharingcar.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
         through: "carsharingCarsImages",
         foreignKey: "carId",
         otherKey: "mediaId",
-        as: "images"
+        as: "images",
       });
     }
   }
@@ -48,6 +48,22 @@ module.exports = (sequelize, DataTypes) => {
       weightAndPayload: {
         type: DataTypes.JSON,
         allowNull: true,
+      },
+      size: {
+        type: DataTypes.ENUM("XXS", "XS", "S", "M", "L", "XL", "XXL"),
+        allowNull: true, // Allow null values
+      },
+      fuel: {
+        type: DataTypes.ENUM("Benzin", "Diesel", "Elektro", "Hybrid"),
+        allowNull: true, // Allow null values
+      },
+      gear: {
+        type: DataTypes.ENUM("Automatik", "Schaltgetriebe"),
+        allowNull: true, // Allow null values
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: true, // Allow null values
       },
     },
     {
