9645c98351d8f6ec376ba84b188bc7989ff0d23d
WhoopsImTop <eliasenglen2@gmail.com>
2026-04-07 14:38:40 +0200

    change to fetch instead of axios

--- controllers/website/newsletterController.js
+++ controllers/website/newsletterController.js
@@ -1,6 +1,3 @@
-const sequelize = require("sequelize");
-const db = require("../../models");
-
 exports.subscribeToNewsletter = async (req, res) => {
   try {
     //check if request was from *gruene-flotte.com domain
@@ -12,23 +9,21 @@ exports.subscribeToNewsletter = async (req, res) => {
       return res.status(400).json({ error: "Invalid email" });
     }
     //make api call to brevo to subscribe to newsletter
-    const response = await axios.post(
-      "https://api.brevo.com/v3/contacts/doubleOptinConfirmation",
-      {
+    const response = await fetch("https://api.brevo.com/v3/contacts/doubleOptinConfirmation", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "api-key": process.env.BREVO_API_KEY,
+      },
+      body: JSON.stringify({
         email: req.body.email,
         includeListIds: [2],
         redirectionUrl: "https://gruene-flotte.com/carsharing",
         templateId: 2,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "api-key": process.env.BREVO_API_KEY,
-        },
-      }
-    );
+      }),
+    });
 
-    if (response.status !== 201 && response.status !== 204) {
+    if (!response.ok || (response.status !== 201 && response.status !== 204)) {
       return res.status(500).json({ error: "Failed to subscribe to newsletter" });
     }
 
